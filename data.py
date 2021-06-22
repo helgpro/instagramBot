e6e70cdaf8ab7ea4fb66037bf1b19558b8d928e6
helgpro <74286617+helgpro@users.noreply.github.com>
2021-06-22 10:25:25 +0600

    Add files via upload

---
+++ data.py
@@ -0,0 +1,16 @@
+username = "olenpro85"
+password = "Abc12ixw38"
+
+direct_users_list = ['https://www.instagram.com/andy1985.kg/']# с кем работаем
+
+users_settings_dict = {
+	'user1': {		  # Боты
+		'login': 'olenpro85',
+		'password': 'Abc12ixw38',
+		'window_size':'800,600'
+	},
+	# 'user2': {
+	#	 'login': 'bot_login',
+	#	 'password': 'bot_password'
+	# }
+}
